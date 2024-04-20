a48b2e32102c38ec22fd4697d2461bc0c0280b55
namigismylv <tu7ew05s4@code.edu.az>
2024-04-20 16:47:08 +0400

    some changes

--- index.js
+++ index.js
@@ -1,23 +1,40 @@
 const toDoApp = document.getElementById("todoapp")
 const add = document.getElementById("add")
 const list = document.getElementById("list")
-
 add.addEventListener("click", (e) => {
-    if (toDoApp.value != "") {
+    if (toDoApp.value.trim() == "") return
         let newLi = document.createElement("li")
         let newBtn = document.createElement("button")
         let p = document.createElement("p")
         let icon = document.createElement("i")
         let checkbox = document.createElement("input")
         let div = document.createElement("div")
-        icon.classList.add("fa-solid", "fa-trash")
         p.innerHTML = toDoApp.value
         list.appendChild(newLi)
         newLi.append(div, newBtn)
         newBtn.append(icon)
         div.append(checkbox, p)
+        icon.classList.add("fa-solid", "fa-trash")
         toDoApp.value = ""
         checkbox.type = "checkbox"
-    }
+        newBtn.addEventListener("click",()=>{
+            let choise=confirm("Silməyinizə əminsiniz?")
+            if(choise)
+            {   
+                newBtn.parentElement.style.display="none"
+            }
+            
+        })        
+        checkbox.addEventListener("click",(e)=>{
+            if (checkbox.checked){
+                newLi.classList.add("completed")
+                newLi.style.opacity="0.5"
+            }
+            else{
+                newLi.classList.remove("completed")
+                newLi.style.opacity="1"
 
-})
+            }
+        })
+        
+    })
